52564673d8ca81fdf60237c6106e036fff1d7538
kalelfleith <kalelfleith03@gmail.com>
2022-02-22 22:36:53 -0300

    function like

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,12 +1,9 @@
-import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import { useRoom } from '../hooks/useRoom';
 import logoImg from '../assets/imgs/logo.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 import '../styles/question.scss';
@@ -16,37 +13,10 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-    const { user } = useAuth();
     const params = useParams<RoomParams>();
-    const [newQuestion, setNewQuestion] = useState('');
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId!);
-
-    async function handleSendQuestion(event: FormEvent) {
-        event.preventDefault();
-        if (newQuestion.trim() === '') {
-            return;
-        }
-
-        if (!user) {
-            throw new Error('You must be logged in');
-        }
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user.name,
-                avatar: user.avatar,
-            },
-            isHighLighted: false,
-            isAnswered: false
-        };
-
-        await database.ref(`rooms/${roomId}/questions`).push(question);
-
-        setNewQuestion('');
-    }
 
     return (
         <div id="page-room">
